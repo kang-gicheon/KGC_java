1a887d7bb5154173c065813e33cf894c6e1db566
kanggicheon <snoopy1516@naver.com>
2023-04-14 17:09:50 +0900

    변수선언, 응용

---
+++ ch02/sec01/VariableUseExample.java
@@ -0,0 +1,16 @@
+package ch02.sec01;
+
+public class VariableUseExample {
+
+	public static void main(String[] args) {
+	
+		int hour = 3;
+		int minute = 5;
+		System.out.println(hour +"시간 " + minute + "분");
+		
+		int TotalMinute = (hour*60) + minute;
+		System.out.println("총 " + TotalMinute + "분");
+
+	}
+
+}
