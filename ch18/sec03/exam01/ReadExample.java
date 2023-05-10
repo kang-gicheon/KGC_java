2ac931378e8e24c3a38c5fa1ccc23bbe68a7b9bf
kanggicheon <snoopy1516@naver.com>
2023-05-10 18:04:02 +0900

    자바 스트림(입출력예제 실습)

---
+++ ch18/sec03/exam01/ReadExample.java
@@ -0,0 +1,35 @@
+package ch18.sec03.exam01;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ReadExample {
+
+	public static void main(String[] args) {
+	
+		try {
+			
+			InputStream is = new FileInputStream("c:/temp/Test1.db");
+			
+			while(true) {
+				
+				int data = is.read();
+				
+				if(data == -1) break;
+				System.out.println(data);
+				
+			}
+			
+			is.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
